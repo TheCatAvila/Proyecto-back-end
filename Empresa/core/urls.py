87d70219510185f33b0bf642613948b96c5ab46c
TheCatAvila <TheCatAvila@gmail.com>
2024-08-20 20:04:17 -0400

    Update: Clase 2

--- Empresa/core/urls.py
+++ Empresa/core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import *
 
+
+#LAS URLS SIRVEN PARA LLAMAR A LAS PÁGINAS
+
 urlpatterns = [
     path('', index, name='index'), #PÁGINA DE INICIO
     path('contacto/', contacto, name='contacto'), #PÁGINA DE CONTACTO
